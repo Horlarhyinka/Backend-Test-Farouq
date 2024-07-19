32c59adfc9bb55529cd8b3634a7602f207bd9375
Horlarhyinka <danijufarouq2003@gmail.com>
2024-07-19 17:27:07 +0100

    test: unit tests for product endpoints

--- app/core/tests/test_product.py
+++ app/core/tests/test_product.py
@@ -22,42 +22,42 @@ class ProductTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
-#     def test_create_product(self):
-#         self.authenticate_staff_user()
-#         url = reverse('products')
-#         data = {
-#             'name': 'Product2',
-#             'price': 20.00,
-#             'categoryId': self.category.id
-#         }
-#         response = self.client.post(url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Product.objects.count(), 2)
+    def test_create_product(self):
+        url = reverse('products')
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer "+self.staff_token)
+        data = {
+            'name': 'Product2',
+            'price': 20.00,
+            'categoryId': self.category.id
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 2)
 
-#     def test_retrieve_product(self):
-#         self.authenticate_user()
-#         url = reverse('product', args=[self.product.id])
-#         response = self.client.get(url, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.data['id'], self.product.id)
+    def test_retrieve_product(self):
+        url = reverse('product', args=[self.product.id])
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer "+self.user_token)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.product.id)
 
-#     def test_update_product(self):
-#         self.authenticate_staff_user()
-#         url = reverse('product', args=[self.product.id])
-#         data = {
-#             'name': 'UpdatedProduct',
-#             'price': 30.00,
-#             'categoryId': self.category.id
-#         }
-#         response = self.client.put(url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.product.refresh_from_db()
-#         self.assertEqual(self.product.name, 'UpdatedProduct')
-#         self.assertEqual(self.product.price, 30.00)
+    def test_update_product(self):
+        url = reverse('product', args=[self.product.id])
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer "+self.staff_token)
+        data = {
+            'name': 'UpdatedProduct',
+            'price': 30.00,
+            'categoryId': self.category.id
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.product.refresh_from_db()
+        self.assertEqual(self.product.name, 'UpdatedProduct')
+        self.assertEqual(self.product.price, 30.00)
 
-#     def test_delete_product(self):
-#         self.authenticate_staff_user()
-#         url = reverse('product', args=[self.product.id])
-#         response = self.client.delete(url, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-#         self.assertFalse(Product.objects.filter(id=self.product.id).exists())
+    def test_delete_product(self):
+        url = reverse('product', args=[self.product.id])
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer "+self.staff_token)
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Product.objects.filter(id=self.product.id).exists())
